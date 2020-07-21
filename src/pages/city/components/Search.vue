219f8ce754998550506352deeba2ec3db0410e68
Pchiawei <429789881@qq.com>
2020-07-21 12:19:35 +0800

    add city-search

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,27 @@
+<template>
+   <div class="search-area">
+       <input class="search-input" type="text" placeholder="请输入城市或拼音" />
+   </div>
+</template>
+
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+
+<style lang="stylus" scoped>
+    @import '~styles/varibles.styl'
+   .search-area
+        background :$bgColor
+        text-align :center
+        padding: .1rem .3rem
+        .search-input
+            height :.5rem
+            width :100%
+            border-radius:.1rem
+            text-align :center
+            color:#666
+            padding : 0 .2rem
+            box-sizing:border-box
+</style>
