0bbcb24b310785bf804ef4e4cfa3a9c291d72d58
Pchiawei <429789881@qq.com>
2020-07-23 14:26:29 +0800

    finish detailHeader

--- src/common/gallery/Gallery.vue
+++ src/common/gallery/Gallery.vue
@@ -44,7 +44,7 @@ export default {
     bottom : 0
     left :0
     right:0
-    background #000
+    background rgba(0, 0, 0, .9)
     display flex
     flex-direction column
     justify-content center
---
+++ src/pages/detail/components/Header.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <router-link tag="div" to="/" class="header-abs" v-show="showAbs"><span class="iconfont abs-back-icon">&#xe658;</span></router-link>
+    <router-link
+    tag="div"
+    to="/"
+    class="header-fixed"
+    v-show="!showAbs"
+    :style="opacityStyle"
+    ><span class="iconfont fixed-back-icon">&#xe658;</span>景点详情</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailHeader',
+  data () {
+    return {
+      showAbs: true,
+      opacityStyle: {
+        opacity: 0
+      }
+    }
+  },
+  methods: {
+    handleScroll () {
+      let top = document.documentElement.scrollTop || document.body.scrollTop
+      if (top > 60) {
+        this.showAbs = false
+        let opacity = top / 140
+        opacity = opacity > 1 ? 1 : opacity
+        this.opacityStyle = { opacity }
+      } else {
+        this.showAbs = true
+      }
+    }
+  },
+  mounted () {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+.header-abs
+    position absolute
+    top .1rem
+    left .1rem
+    background #000
+    width .8rem
+    height .8rem
+    line-height  .8rem
+    border-radius .4rem
+    color #fff
+    text-align center
+    .abs-back-icon
+        font-size .4rem
+.header-fixed
+    position fixed
+    top 0
+    left 0
+    width 100%
+    height : $headerHeight
+    line-height: $headerHeight
+    text-align :center
+    color :#fff
+    font-size : .32rem
+    background : $bgColor
+    .fixed-back-icon
+        position :absolute
+        left: .2rem
+        top:0
+        font-size : .5rem
+</style>
