65bf54e95d3e5a5393904b9aa6f447a15134ee45
ChiaweiPeng <chiawei429789881@163.com>
2021-03-03 13:19:25 +0800

    修改打包配置

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,12 @@
 const path = require('path')
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/project'
-    : '/',
+  assetsDir: 'static',
+  parallel: false,
+  publicPath: './',
+  // publicPath: process.env.NODE_ENV === 'production'
+  //   ? '/project'
+  //   : '/',
   // devServer: {
   //   proxy: {
   //     '/api': {
@@ -12,11 +15,11 @@ module.exports = {
   //     }
   //   }
   // },
-  devServer:{
+  devServer: {
     proxy: {
       '/api': {
         target: 'http://111.229.100.170:8003',
-        changeOrigin:true,
+        changeOrigin: true,
         pathRewrite: {
           // 要加重定向
           '^/api': '/travel-mock'
